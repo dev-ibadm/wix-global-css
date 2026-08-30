f1f15c3a283a6959a8cc58aa0a8176da4825ef82
dev-ibadm <ibadmujahid58@gmail.com>
2026-08-30 22:54:25 +0500

    Delete mobile menu event listener script
    Remove mobile menu script from global.js

--- public/global.js
+++ public/global.js
@@ -1,16 +1,1 @@
-<script>
-window.addEventListener('message', function(e) {
-  if (e.data && e.data.type === 'MOBILE_MENU') {
-    if (window.innerWidth <= 849) {
-      var el = document.querySelector('.comp-mb7ogqrp_r_comp-mdr142kt');
-      if (el) {
-        if (e.data.action === 'open') {
-          el.style.setProperty('height', '100%', 'important');
-        } else {
-          el.style.setProperty('height', '80px', 'important');
-        }
-      }
-    }
-  }
-});
-</script>
+
